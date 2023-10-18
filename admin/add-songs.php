4f1392382a76ebb17f2067be3dbf4ae5ecc79d4e
vernonthedev <techjaja2@gmail.com>
2023-10-18 05:14:06 -0700

    #Update: done adding song fields

--- admin/add-songs.php
+++ admin/add-songs.php
@@ -5,7 +5,7 @@ include'config.php';
 <html lang="en">
 <head>
     <meta charset="utf-8" />
-    <title>Admin, Add Events | EKC SDA Church</title>
+    <title>Admin, Add Songs | EKC SDA Church</title>
     <?php include'style.php'; ?>
 </head>
 
@@ -26,12 +26,12 @@ include'config.php';
                 <div class="row">
                     <div class="col-12">
                         <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                            <h4 class="mb-sm-0 font-size-18">Add Event</h4>
+                            <h4 class="mb-sm-0 font-size-18">Add Hymns</h4>
 
                             <div class="page-title-right">
                                 <ol class="breadcrumb m-0">
                                     <li class="breadcrumb-item"><a href="./">Admin</a></li>
-                                    <li class="breadcrumb-item active">Add Event</li>
+                                    <li class="breadcrumb-item active">Add Hymns</li>
                                 </ol>
                             </div>
 
@@ -44,43 +44,64 @@ include'config.php';
                     <div class="col-12">
                         <div class="card">
                             <div class="card-header">
-                                <h4 class="card-title">Add Event</h4>
+                                <h4 class="card-title">Add Hymns</h4>
                             </div>
                             <div class="card-body p-4">
 
                                 <div class="row">
                                     <div class="col-lg-6">
                                         <form action="" method="POST" enctype="multipart/form-data" >
+
                                             <div class="mb-3">
-                                                <label for="example-text-input" class="form-label">Event Title:</label>
-                                                <input class="form-control" type="text"  id="example-text-input" name="e_title" placeholder="Event Title">
+                                                <label for="introit" class="form-label">Introit:</label>
+                                                <textarea class="form-control" rows="8" type="text"  id="introit" name="introit" placeholder="Introit Content"></textarea>
                                             </div>
 
                                             <div class="mb-3">
-                                                <label for="example-text-input" class="form-label">Event Date:</label>
-                                                <input class="form-control" type="date"  id="example-text-input" name="e_date" placeholder="Event Date">
+                                                <label for="callto" class="form-label">Call to Worship:</label>
+                                                <textarea class="form-control" rows="8" type="text"  id="callto" name="callto" placeholder="Call to Worship Content"></textarea>
                                             </div>
 
                                             <div class="mb-3">
-                                                <label for="example-text-input" class="form-label">Event Place:</label>
-                                                <input class="form-control" type="text"  id="example-text-input" name="e_place" placeholder="Event Place">
+                                                <label for="dox" class="form-label">Doxology:</label>
+                                                <textarea class="form-control" rows="8" type="text"  id="dox" name="dox" placeholder="Doxology"></textarea>
                                             </div>
 
                                             <div class="mb-3">
-                                                <label for="example-text-input" class="form-label">Event Image:</label>
-                                                <input class="form-control" type="file"  id="example-img-input" name="e_img" placeholder="Event Image">
+                                                <label for="welcome" class="form-label">Welcome Song: </label>
+                                                <textarea class="form-control" rows="8" type="text"  id="welcome" name="welcome" placeholder="Welcoming song"></textarea>
                                             </div>
 
                                             <div class="mb-3">
-                                                <label for="example-text-input" class="form-label">Event Content:</label>
-                                                <textarea class="form-control" rows="8" type="text"  id="example-msg-input" name="e_content" placeholder="Event Content"></textarea>
+                                                <label for="opening" class="form-label">Opening Hymn:</label>
+                                                <textarea class="form-control" rows="8" type="text"  id="opening" name="opening" placeholder="Opening Song"></textarea>
+                                            </div>
+
+                                            <div class="mb-3">
+                                                <label for="prayerSong" class="form-label">Prayer Song:</label>
+                                                <textarea class="form-control" rows="8" type="text"  id="prayerSong" name="prayerSong" placeholder="Prayer Song"></textarea>
+                                            </div>
+
+                                            <div class="mb-3">
+                                                <label for="mem" class="form-label">Memory Text:</label>
+                                                <textarea class="form-control" rows="8" type="text"  id="mem" name="mem" placeholder="Memory Text"></textarea>
+                                            </div>
+
+                                            <div class="mb-3">
+                                                <label for="closing" class="form-label">Closing Hymn:</label>
+                                                <textarea class="form-control" rows="8" type="text"  id="closing" name="closing" placeholder="Closing Song"></textarea>
+                                            </div>
+
+                                            <div class="mb-3">
+                                                <label for="affirm" class="form-label">Affirmation Hymn:</label>
+                                                <textarea class="form-control" rows="8" type="text"  id="affirm" name="affirm" placeholder="Affirmation Song"></textarea>
                                             </div>
 
                                             <div class="mt-4">
                                                 <button type="submit" name="submit" class="btn btn-primary w-md"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-upload" viewBox="0 0 16 16">
                                                         <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
                                                         <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z"/>
-                                                    </svg> Upload Event</button>
+                                                    </svg> Upload Divine Songs</button>
                                             </div>
 
                                         </form>
