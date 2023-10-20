f0656922ee7f0044e60732e698fb004e7bb804df
vernonthedev <techjaja2@gmail.com>
2023-10-20 02:04:59 -0700

    #Update: added some seo

--- head.php
+++ head.php
@@ -1,21 +1,22 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>East Kampala SDA Church</title>
-    <meta name="description" content="">
-    <meta name="keywords" content="">
-    <meta name="author" content="">
+    <title>East Kampala Seventh Day Adventist Church</title>
+    <meta name="description" content="Welcome to East Kampala Seventh Day Adventist Church - a vibrant community of faith">
+    <meta name="keywords" content="East Kampala SDA Church, Seventh Day Adventist, East Kampala, SDA Church, Adventist Church, worship, community, EKC, ekc, Uganda Seventh Day Adventist Churches, ">
+    <meta name="author" content="East Kampala Seventh Day Adventist Church">
     <link rel="shortcut icon" href="images/sda%20logo%20black.png">
-    <!-- Mobile Specific Metas
-      ================================================== -->
+
+    <!-- Mobile Specific Metas -->
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
     <meta name="format-detection" content="telephone=no">
-    <!-- CSS
-      ================================================== -->
+
+    <!-- CSS -->
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
-    <link href="plugins/mediaelement/mediaelementplayer.css" rel="stylesheet" type="text/css">
+    <link href="plugins/mediaelement/mediaelementplayer.css" rel="stylesheet" type="text_css">
     <link href="css/style.css" rel="stylesheet" type="text/css">
     <link href="plugins/prettyphoto/css/prettyPhoto.css" rel="stylesheet" type="text/css">
-    <!--[if lte IE 8]><link rel="stylesheet" type="text/css" href="css/ie8.css" media="screen" /><![endif]-->
+    <link rel="stylesheet" type="text/css" href="css/ie8.css" media="screen" />
+
     <!-- Color Style -->
     <link href="colors/color1.css" rel="stylesheet" type="text/css">
     <link href="css/custom.css" rel="stylesheet" type="text/css">
